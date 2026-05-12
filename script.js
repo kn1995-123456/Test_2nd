3c19f7676b219901d576d556e032ef88d3fef3b0
kn1995 <283101794+kn1995-123456@users.noreply.github.com>
2026-05-12 11:05:57 +0900

    TOOD App

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-const button = document.getElementById("changeButton");
+const greetButton =
+  document.getElementById("greetButton");
 
-const message = document.getElementById("message");
+const nameInput =
+  document.getElementById("nameInput");
 
-button.addEventListener("click", () => {
-  message.textContent = "ボタンが押されました！";
+const greetingMessage =
+  document.getElementById("greetingMessage");
 
-  message.style.color = "red";
+greetButton.addEventListener("click", () => {
+
+  const name = nameInput.value;
+
+  greetingMessage.textContent =
+    `こんにちは ${name}さん`;
+
+});
+
+const todoInput =
+  document.getElementById("todoInput");
+
+const addTodoButton =
+  document.getElementById("addTodoButton");
+
+const todoList =
+  document.getElementById("todoList");
+
+addTodoButton.addEventListener("click", () => {
+
+  const task = todoInput.value;
+
+  if (task === "") {
+    return;
+  }
+
+  const li =
+    document.createElement("li");
+
+  li.textContent = task;
+
+  todoList.appendChild(li);
+
+  todoInput.value = "";
+
 });
